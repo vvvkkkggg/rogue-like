bb046a2657c3311d1335b88249a0adb41d83210a
a1k0u <alko7enko@gmail.com>
2024-06-03 03:21:04 +0300

    fear(monsters): [TASK-04] add Mold mosnter with Prototype pattern and collision strategy

--- model/monster.py
+++ model/monster.py
@@ -1,6 +1,9 @@
 from model.position import Position
-from model.strategy import AggressiveStrategy, PassiveStrategy, CowardlyStrategy
+from model.strategy import AggressiveStrategy, PassiveStrategy, CowardlyStrategy, SameTypeCollisionAvoidanceStrategy
 import pygame
+import copy
+import random
+
 
 class Monster:
     """
@@ -12,8 +15,12 @@ class Monster:
         hp (int): The health points of the monster.
         attack (int): The attack strength of the monster.
         defense (int): The defense strength of the monster.
+        copy_probability(float): Probability of monster clone.
     """
-    def __init__(self, position, strategy, hp, attack, defense, speed, distance):
+
+    def __init__(
+        self, position, strategy, hp, attack, defense, speed, distance, copy_probability
+    ):
         """
         Initializes a new instance of the Monster class.
 
@@ -31,8 +38,9 @@ class Monster:
         self.defense = defense
         self.speed = speed
         self.distance = distance
+        self.copy_probability = copy_probability
 
-    def move(self, field, player_position):
+    def move(self, field, player_position, monsters=None):
         """
         Moves the monster according to its strategy.
 
@@ -40,7 +48,7 @@ class Monster:
             field (Field): The game field where the monster is moving.
             player_position (Position): The current position of the player.
         """
-        self.strategy.move(self, field, player_position)
+        self.strategy.move(self, field, player_position, monsters)
 
     def attack_player(self, player):
         """
@@ -62,15 +70,76 @@ class Monster:
         """
         self.hp -= damage * 0.1
 
+    def should_clone(self):
+        """
+        Determines whether to clone the monster based on the probability.
+
+        Returns:
+            bool: True if the monster should be cloned, False otherwise.
+        """
+        return random.random() < self.copy_probability
+
+    def clone(self):
+        """
+        Creates a copy of this Monster instance.
+
+        Returns:
+            Monster: A new instance of the Monster class with the same attributes.
+        """
+        return copy.deepcopy(self)
+
 
 class AggressiveMonster(Monster):
     def __init__(self, position):
-        super().__init__(position, AggressiveStrategy(), hp=30, attack=20, defense=5, speed = 0.2, distance = 4)
+        super().__init__(
+            position,
+            AggressiveStrategy(),
+            hp=30,
+            attack=20,
+            defense=5,
+            speed=0.2,
+            distance=4,
+            copy_probability=-1,
+        )
+
 
 class PassiveMonster(Monster):
     def __init__(self, position):
-        super().__init__(position, PassiveStrategy(), hp=20, attack=5, defense=10, speed = 0, distance = 1)
+        super().__init__(
+            position,
+            PassiveStrategy(),
+            hp=20,
+            attack=5,
+            defense=10,
+            speed=0,
+            distance=1,
+            copy_probability=-1,
+        )
+
 
 class CowardlyMonster(Monster):
     def __init__(self, position):
-        super().__init__(position, CowardlyStrategy(), hp=15, attack=5, defense=5, speed = 0.1, distance = 4)
+        super().__init__(
+            position,
+            CowardlyStrategy(),
+            hp=15,
+            attack=5,
+            defense=5,
+            speed=0.1,
+            distance=4,
+            copy_probability=-1,
+        )
+
+
+class MoldMonster(Monster):
+    def __init__(self, position):
+        super().__init__(
+            position,
+            SameTypeCollisionAvoidanceStrategy(),
+            hp=1,
+            attack=5,
+            defense=0,
+            speed=0,
+            distance=4,
+            copy_probability=0.01,
+        )
