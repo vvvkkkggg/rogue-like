98ae39229559ce365ec56bcaab61627743a9ff16
a1k0u <alko7enko@gmail.com>
2024-06-03 03:21:04 +0300

    fear(monsters): [TASK-04] add Mold mosnter with Prototype pattern and collision strategy

--- model/strategy.py
+++ model/strategy.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 class Strategy:
     """
@@ -8,7 +9,7 @@ class Strategy:
         move(monster, field, player_position): Defines the movement logic for the monster.
     """
 
-    def move(self, monster, field, player_position):
+    def move(self, monster, field, player_position, monsters=None):
         """
         Defines the movement logic for the monster.
 
@@ -28,7 +29,7 @@ class RandomStrategy(Strategy):
         move(monster, field, player_position): Moves the monster randomly on the field.
     """
 
-    def move(self, monster, field, player_position):
+    def move(self, monster, field, player_position, monsters=None):
         """
         Moves the monster randomly on the field.
 
@@ -48,7 +49,7 @@ class PassiveStrategy(Strategy):
         move(monster, field, player_position): Moves the monster passively on the field.
     """
 
-    def move(self, monster, field, player_position):
+    def move(self, monster, field, player_position, monsters=None):
         """
         Moves the monster passively on the field.
 
@@ -68,7 +69,7 @@ class AggressiveStrategy(Strategy):
         move(monster, field, player_position): Moves the monster aggressively on the field.
     """
 
-    def move(self, monster, field, player_position):
+    def move(self, monster, field, player_position, monsters=None):
         """
         Moves the monster aggressively towards the player on the field.
 
@@ -78,8 +79,10 @@ class AggressiveStrategy(Strategy):
             player_position (Position): The current position of the player.
         """
 
-        distance = (player_position.x - monster.position.x) ** 2 + (player_position.y - monster.position.y) ** 2
-        if distance <= monster.distance ** 2:
+        distance = (player_position.x - monster.position.x) ** 2 + (
+            player_position.y - monster.position.y
+        ) ** 2
+        if distance <= monster.distance**2:
             if player_position.x > monster.position.x:
                 monster.position.x += monster.speed
             elif player_position.x < monster.position.x:
@@ -98,7 +101,7 @@ class CowardlyStrategy(Strategy):
         move(monster, field, player_position): Moves the monster away from the player on the field.
     """
 
-    def move(self, monster, field, player_position):
+    def move(self, monster, field, player_position, monsters=None):
         """
         Moves the monster away from the player on the field.
 
@@ -108,8 +111,10 @@ class CowardlyStrategy(Strategy):
             player_position (Position): The current position of the player.
         """
 
-        distance = (player_position.x - monster.position.x) ** 2 + (player_position.y - monster.position.y) ** 2
-        if distance <= monster.distance ** 2:
+        distance = (player_position.x - monster.position.x) ** 2 + (
+            player_position.y - monster.position.y
+        ) ** 2
+        if distance <= monster.distance**2:
             if player_position.x > monster.position.x:
                 monster.position.x -= monster.speed
             elif player_position.x < monster.position.x:
@@ -118,3 +123,69 @@ class CowardlyStrategy(Strategy):
                 monster.position.y -= monster.speed
             elif player_position.y < monster.position.y:
                 monster.position.y += monster.speed
+
+
+class SameTypeCollisionAvoidanceStrategy(Strategy):
+    """
+    The SameTypeCollisionAvoidanceStrategy class represents a strategy for avoiding collisions with monsters of the same type.
+
+    Methods:
+        get_near_monster_position(monster): Generates positions near the given monster.
+        move(monster, field, player_position, monsters): Moves the monster to avoid collisions with monsters of the same type.
+
+    Attributes:
+        None
+    """
+
+    def get_near_monster_position(self, monster):
+        """
+        Generates positions near the given monster.
+
+        Args:
+            monster (Monster): The monster instance.
+
+        Yields:
+            tuple: A tuple representing a position near the monster.
+        """
+
+        for x in random.sample([-1, 0, 1], k=3):
+            for y in random.sample([-1, 0, 1], k=3):
+                yield monster.position.x + x, monster.position.y + y
+
+    def move(self, monster, field, player_position, monsters):
+        """
+        Moves the monster to avoid collisions with monsters of the same type.
+
+        Args:
+            monster (Monster): The monster instance.
+            field (Field): The game field.
+            player_position (Position): The current position of the player.
+            monsters (list): A list of all monsters on the field.
+        """
+
+        if not monsters:
+            return
+        
+        monsters_positions = map(
+            lambda monster: (monster.position.x, monster.position.y),
+            filter(
+                lambda another_monster: another_monster is not monster
+                and type(another_monster) == type(monster),
+                monsters,
+            ),
+        )
+
+        if (monster.position.x, monster.position.y) not in monsters_positions:
+            return
+
+        for x, y in self.get_near_monster_position(monster):
+            if (x, y) in monsters_positions:
+                continue
+            
+            if not (0 <= x <= field.width and 0 <= y <= field.height):
+                continue
+
+            monster.position.x = x
+            monster.position.y = y
+
+            return
